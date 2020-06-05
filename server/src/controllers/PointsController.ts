dd7f7ff4f5deca6953aab51087cd6b59115f3f1e
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-06-04 23:50:58 -0300

    Finalizando o BackEnd

---
+++ server/src/controllers/PointsController.ts
@@ -0,0 +1,111 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class PointsController {
+
+    async create (request: Request, response: Response) {
+        const {
+            name,
+            email,
+            whatsapp, 
+            latitude, 
+            longitude, 
+            city, 
+            uf, 
+            items
+        } = request.body;
+    
+        const trx = await knex.transaction();
+
+        const point = {
+            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            name,
+            email,
+            whatsapp, 
+            latitude, 
+            longitude, 
+            city, 
+            uf
+        }
+    
+        const insertedIds = await trx('points').insert(point);
+    
+        const point_id = insertedIds[0];
+    
+        const pointItems = items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id,
+            }
+        })
+    
+        await trx('point_items').insert(pointItems);
+
+        await trx.commit();
+    
+        return response.json({
+            id: point_id,
+            ...point,
+        });
+    }
+
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+
+        const point = await knex('points').where('id', id).first();
+
+        if(!point){
+            return response.status(400).json({ message: "Point not found."});            
+        }
+
+        const items = await knex('items')
+        .join('point_items', 'items.id', '=', 'point_items.item_id')
+        .where('point_items.point_id', id)
+        .select('items.title');
+
+        return response.json({point, items});
+    }
+
+    async index(request: Request, response: Response) {
+        const { city, uf, items } = request.query;
+
+        const parsedItems = String(items)
+        .split(',')
+        .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+        .join('point_items', 'points.id', '=', 'point_items.point_id')
+        .whereIn('point_items.item_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('points.*');
+
+        return response.json(points);
+    }
+
+   /* async delete(request: Request, response: Response){
+        const { id } = request.params;
+
+        //delete from point_items where point_id = id;
+
+        const trx = await knex.transaction();
+
+        const selectPoint = await trx('points').where('id', id);
+
+        if(!selectPoint){
+            return response.json({message: "O point não está cadastrad."});s
+        }
+        
+        await trx('point_items').delete('item_id').where('point_id', id);
+        
+        await trx('points').delete(id);
+
+        await trx.commit();
+
+        return response.json({message: "Dados deletados."});
+    }*/
+    
+}
+
+export default PointsController;
